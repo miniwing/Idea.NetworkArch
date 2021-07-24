630204e06d3c1c0d51cdbb0a21127ec28fd110c3
Harry <miniwing.hz@gmail.com>
2021-07-24 17:05:47 +0800

    添加 TodayWidget

---
+++ NetworkArch/TodayWidget/TodayViewController.m
@@ -0,0 +1,33 @@
+//
+//  TodayViewController.m
+//  TodayWidget
+//
+//  Created by Harry on 2021/7/24.
+//  Copyright © 2021 Harry. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+@end
